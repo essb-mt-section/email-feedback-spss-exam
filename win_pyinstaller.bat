f25d259fe3b7d2bf2718642f8807d0e12086648f
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-03-18 17:48:44 +0100

    v0.7.8

--- win_pyinstaller.bat
+++ win_pyinstaller.bat
@@ -1,4 +1,5 @@
 python -m virtualenv env_efse
 
 .\env_efse\Scripts\pip.exe install pandas, mailcomposer, markdown, appdirs, pysimplegui, pyinstaller
-.\env_efse\Scripts\pyinstaller.exe --onefile -w email_feedback_spss_exam.py
+.\env_efse\Scripts\pyinstaller.exe --onefile -w --icon=picts\ESSB_logo.ico
+email_feedback_spss_exam.py
