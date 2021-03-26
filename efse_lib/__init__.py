1fc26f1ae5375f7a45d285e39101df5570d36ea8
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-03-26 10:36:56 +0100

    Python3 only

--- efse_lib/__init__.py
+++ efse_lib/__init__.py
@@ -1,9 +1,18 @@
 __version__ = "0.7.8"
 __author__ = "Oliver Lindemann"
 
-from . import json_settings, const
-settings = json_settings.JSONSettings(appname=
-                        const.APPNAME.replace(" ","_").lower(),
+import sys as _sys
+from . import const
+if _sys.version_info[0] != 3 or _sys.version_info[1]<5:
+
+    raise RuntimeError("{} {} ".format(const.APPNAME, __version__) +
+                      "is not compatible with Python {0}.{1}.".format(
+                                                    _sys.version_info[0],
+                                                    _sys.version_info[1]) +
+                      "\n\nPlease use Python 3.5 or higher.")
+
+from .json_settings import JSONSettings as _JSONSettings
+settings = _JSONSettings(appname=const.APPNAME.replace(" ","_").lower(),
                         settings_file_name="settings.json",
                         defaults=const.DEFAULT_SETTINGS)
 
--- win_pyinstaller.bat
+++ win_pyinstaller.bat
@@ -1,5 +1,4 @@
 python -m virtualenv env_efse
 
 .\env_efse\Scripts\pip.exe install pandas, mailcomposer, markdown, appdirs, pysimplegui, pyinstaller
-.\env_efse\Scripts\pyinstaller.exe --onefile -w --icon=picts\ESSB_logo.ico
-email_feedback_spss_exam.py
+.\env_efse\Scripts\pyinstaller.exe --onefile -w --icon=picts\ESSB_logo.ico email_feedback_spss_exam.py
